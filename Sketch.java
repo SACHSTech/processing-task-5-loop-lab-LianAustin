12c4fe11e0a22cbe9cc0575f3cf85eb353a9aefe
LianAustin <austin.lian25@ycdsbk12.ca>
2024-03-24 18:55:12 -0400

    Completed section 5

--- Sketch.java
+++ Sketch.java
@@ -163,7 +163,17 @@ public class Sketch extends PApplet {
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
-
+    int intX = 0;
+    int intY = 0;
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 30 - intRow; intColumn < 30; intColumn++) {
+        intX = 3 + intRow * 10;
+        intY = 3 + intColumn * 10;
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
 
   public void draw_section6(){
